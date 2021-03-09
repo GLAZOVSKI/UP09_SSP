7d0e9d1266695df99ab9924537368568dc7e6b34
GLZVSKI <61746157+GLAZOVSKI@users.noreply.github.com>
2021-03-09 11:25:18 +0700

    Added main page

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -7,7 +7,17 @@
     <title>Главная</title>
   </head>
   <body>
-    <h1>Hello world!</h1>
-    <?php include 'application/views/'.$content_view; ?>
+    <div class="container">
+      <header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-body border-bottom">
+        <p class="h5 my-0 me-md-auto fw-normal">GLZVSKI LTD</p>
+        <nav class="my-2 my-md-0 me-md-3">
+          <a class="p-2 text-dark" href="/">Home</a>
+          <a class="p-2 text-dark" href="/portfolio">Portfolio</a>
+        </nav>
+        <a class="btn btn-outline-primary" href="#">Sign up</a>
+      </header>
+
+      <?php include 'application/views/'.$content_view; ?>
+    </div>
   </body>
 </html>

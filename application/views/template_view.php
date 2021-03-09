dce0a21a4908164243ec7b99ee568189cbdbe85e
GLZVSKI <61746157+GLAZOVSKI@users.noreply.github.com>
2021-03-09 11:11:39 +0700

    default project

---
+++ application/views/template_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js" charset="utf-8"></script>
+    <title>Главная</title>
+  </head>
+  <body>
+    <h1>Hello world!</h1>
+    <?php include 'application/views/'.$content_view; ?>
+  </body>
+</html>

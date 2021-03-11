2623efc7e6f8482edc40076a8ad356f69ebde5fc
GLZVSKI <61746157+GLAZOVSKI@users.noreply.github.com>
2021-03-12 00:47:08 +0700

    update DB

--- ssp.sql
+++ ssp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Мар 09 2021 г., 15:23
+-- Время создания: Мар 11 2021 г., 20:46
 -- Версия сервера: 10.4.12-MariaDB
 -- Версия PHP: 7.4.14
 
@@ -43,6 +43,35 @@ INSERT INTO `portfolio` (`id`, `year`, `site`, `description`) VALUES
 (2, 2012, 'http://ZopoMobile.ru', 'Русскоязычный каталог китайских телефонов компании Zopo на базе Android OS и аксессуаров к ним.'),
 (3, 2021, 'http://ssp/portfolio', 'Тестовая запись');
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `passwordSalt` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstName` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lastName` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `middleName` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `admin` int(1) NOT NULL DEFAULT 0,
+  `createdTime` datetime NOT NULL DEFAULT current_timestamp(),
+  `updateTime` datetime NOT NULL DEFAULT current_timestamp(),
+  `blocked` int(1) NOT NULL DEFAULT 0,
+  `attempt` int(1) NOT NULL DEFAULT 0,
+  `blockedTime` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `passwordSalt`, `firstName`, `lastName`, `middleName`, `admin`, `createdTime`, `updateTime`, `blocked`, `attempt`, `blockedTime`) VALUES
+(7, 'glzvski@gmail.com', 'acadb0723173154b249117f57e512700a4a92f903013f3431aa381ea5e8aa945', '5uRHB2SJbHmZDFiwWJQh', 'Владислав', 'Глазов', 'Владимирович', 0, '2021-03-11 22:23:59', '2021-03-11 22:23:59', 0, 0, NULL);
+
 --
 -- Индексы сохранённых таблиц
 --
@@ -54,6 +83,12 @@ ALTER TABLE `portfolio`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -62,6 +97,12 @@ ALTER TABLE `portfolio`
 --
 ALTER TABLE `portfolio`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
